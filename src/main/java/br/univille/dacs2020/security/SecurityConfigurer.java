cfacda5b438724e475a2b5b4de85c060cb32e17e
Walter Coan <waltercoan@hotmail.com>
2020-08-27 20:05:34 +0000

    geração JWT

--- src/main/java/br/univille/dacs2020/security/SecurityConfigurer.java
+++ src/main/java/br/univille/dacs2020/security/SecurityConfigurer.java
@@ -2,7 +2,9 @@ package br.univille.dacs2020.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -22,6 +24,18 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests().antMatchers("/api/v1/auth/signin").permitAll()
+            .anyRequest().authenticated();
+             
+    }
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
 }
